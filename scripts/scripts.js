95a2f328d2e068ffc002bcf4c264526d9a128130
Philip Renich <philip.renich@webtrends.com>
2014-09-04 16:11:58 -0700

    more changes, lost track where. oops

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
+	// $('.headshot').hover(showGus, hideGus);
+
 });
+
+// function showGus() {
+// 	var $headshot = $('.headshot'),
+// 		headshotOffset = $headshot.offset(),
+// 		headshotWidth = $headshot.width(),
+// 		headshotHeight = $headshot.height();
+
+// 	var	gusOffset = {
+// 		top: (headshotHeight / 2) + headshotOffset.top - 20,
+// 		left: headshotWidth / 2
+// 	};
+
+// 	$('.gus').offset(gusOffset).fadeIn('slow');
+// }
+// function hideGus() {
+// 	$('.gus').fadeOut('fast', function() {
+// 		this.style.top = '14px';
+// 		this.style.left = '14px';
+// 	});
+// }
