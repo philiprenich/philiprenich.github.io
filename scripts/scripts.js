4205e702a8298c152d0147b1076ef681d3c5ff31
Philip Renich <philip.renich@webtrends.com>
2014-10-18 13:22:26 -0700

    Better responsive styles (I think that's this update). Contact scripts beta

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,42 @@
+var url = 'http://api.philiprenich.com/contact.php';
 $(document).ready(function() {
+	$.get(url, function(page) {
+		var $contact = $(page).find('#contact');
+		$('#wrap').after($contact);
 
-	// $('.headshot').hover(showGus, hideGus);
 
+		$('a[href="contact.html"]').click(function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+
+			$contact.fadeIn();
+		});
+		$contact.click(function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			if(e.target != e.currentTarget && e.target != $('[href=#cancel]')[0]) return;
+
+			$contact.fadeOut('fast');
+		});
+		$contact.find('[name=send]').click(function(e) {
+			var data = {
+				'email': $contact.find('email').val(),
+				'name': $contact.find('name').val(),
+				'message': $contact.find('message').val(),
+				'send': $(this).val()
+			};
+			$.post(url, data, function(response) {
+				response = JSON.parse(response);
+				if(response.success) {
+					var $form = $contact.find('form').clone();
+					$contact.find('form').empty().append(response.text);
+					setTimeout(function() {
+						$contact.fadeOut(function() {
+							$contact.empty().append($form);
+						});
+					}, 5000);
+				}
+			});
+		});
+	});
 });
-
-// function showGus() {
-// 	var $headshot = $('.headshot'),
-// 		headshotOffset = $headshot.offset(),
-// 		headshotWidth = $headshot.width(),
-// 		headshotHeight = $headshot.height();
-
-// 	var	gusOffset = {
-// 		top: (headshotHeight / 2) + headshotOffset.top - 20,
-// 		left: headshotWidth / 2
-// 	};
-
-// 	$('.gus').offset(gusOffset).fadeIn('slow');
-// }
-// function hideGus() {
-// 	$('.gus').fadeOut('fast', function() {
-// 		this.style.top = '14px';
-// 		this.style.left = '14px';
-// 	});
-// }
